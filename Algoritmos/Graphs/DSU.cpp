bf3ef4952b4b1d928cf7a35f03df3966e25fd1a0
SergioMmCc <138743635+SergioMmCc@users.noreply.github.com>
2024-10-21 14:23:53 -0500

    Add DSU and fix exercise M, TC ARG 2024, dia 3

---
+++ Algoritmos/Graphs/DSU.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+
+const int maxn = 200005;
+int components;
+vector<int> graph[maxn];
+vector<int> leader(maxn);
+vector<int> sets[maxn];
+
+void initDSU(int n){
+    components = n;
+    for(int i = 1; i <= n; i++){
+        leader[i] = i;
+        sets[i].push_back(i);
+    }
+}
+
+void join(int u, int v){
+    int leaderU = leader[u], leaderV = leader[v];
+    if(leaderU != leaderV){
+        if(sets[leaderV].size() > sets[leaderU].size())
+            swap(leaderU, leaderV);
+        
+        for(int i = 0; i < sets[leaderV].size(); i++){
+            int v = sets[leaderV][i];
+            leader[v] = leaderU;
+            sets[leaderU].push_back(v);
+        }
+        sets[leaderV].clear();
+        components--;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(NULL);
+    int n, m;cin>>n>>m;
+    initDSU(n); //Inicializar DSU
+    for(int i = 0; i < m; i++){
+        int u, v;cin>>u>>v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+        join(u, v); //Unir vertice u con v
+    }
+    cout<<components<<endl;
+
+    return 0;
+}
--- Training Camp/Dia 3/M.cpp
+++ Training Camp/Dia 3/M.cpp
@@ -1,62 +1,97 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define MAXT 200000
+#define endl '\n'
 
-vector<set<int>> G(MAXT+1);
-queue<int> q;
-vector<int> sons(MAXT+1);
-vector<int> visited(MAXT+1, 0);
-vector<vector<int>> grupo;
-vector<pair<int, int>> ans;
+const int maxn = 200005;
+int components;
+vector<int> graph[maxn];
+vector<int> leader(maxn);
+vector<int> sets[maxn];
+vector<bool> leaders(maxn, true);
+vector<bool> visited(maxn);
 
-void componentes(int start) {
-    int u;
-    q.push(start);
-    while(!q.empty()) {
-        u = q.front();
-        q.pop();
-        for(int i : G[u]) {
-            if(visited[i] == 1)
-                continue;
-            else {
-                if(i == 1) {
-                    sons[i] = 1;
-                    grupo[]
-                    continue;
-                } 
-                q.push(i);
-            }
+void initDSU(int n){
+    components = n;
+    for(int i = 1; i <= n; i++){
+        leader[i] = i;
+        sets[i].push_back(i);
+    }
+}
+
+void join(int u, int v){
+    int leaderU = leader[u], leaderV = leader[v];
+    if(leaderU != leaderV){
+        if(sets[leaderV].size() > sets[leaderU].size())
+            swap(leaderU, leaderV);
+        
+        for(int i = 0; i < sets[leaderV].size(); i++){
+            int v = sets[leaderV][i];
+            leader[v] = leaderU;
+            sets[leaderU].push_back(v);
+        }
+        sets[leaderV].clear();
+        leaders[leaderV] = 0;
+        components--;
+    }
+}
+
+void DFS(int s){
+    visited[s] = 1;
+    for(int v : graph[s]){
+        if(!visited[v]){
+            join(s, v);
+            DFS(v);
         }
     }
 }
 
-int main() {
-    int n, m, d, u, v;
-    int nComp = 0;
-    cin >> n >> m >> d;
-    for (int i = 0; i < m; i++) {
-        cin >> u >> v;
-        G[v].insert(u);
-        G[u].insert(v);
-    }
-
-    for(int i : G[1]) {
-        if(sons[i] == 1)
-            continue;
-        else {
-            nComp++;
-            grupo.push_back(vector<int>(1, i));
-            componentes(i, &nComp);
+void DFS2(int s){
+    visited[s] = 1;
+    for(int v : graph[s]){
+        if(!visited[v]){
+            cout<<s<<' '<<v<<endl;
+            DFS2(v);
         }
     }
+}
 
-    if(nSons != d)
-        cout << "NO\n";
-    else {
-        cout << "YES\n";
-        for(int i = 0; i < ans.size(); i++)
-            cout << ans[i].first << " " << ans[i].second << "\n";
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(NULL);
+    int n, m, d; cin>>n>>m>>d;
+    initDSU(n);
+    for(int i = 0; i < m; i++){
+        int u, v;cin>>u>>v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+    visited[1] = 1;
+    for(int u : graph[1])
+        DFS(u);
+    components--;
+    if(components > d || graph[1].size() < d){
+        cout<<"NO"<<endl;
+        return 0;
     }
 
+    cout<<"YES"<<endl;
+    for(int i = 2; i <= n; i++)visited[i] = 0;
+    int used = 0;
+    for(int u : graph[1]){
+        if(leaders[u]){
+            visited[u] = 1;
+            used++;
+            cout<<"1 "<<u<<endl;
+        }
+    }
+    for(int u : graph[1]){
+        if(used == d)break;
+        if(!leaders[u]){
+            visited[u] = 1;
+            used++;
+            cout<<"1 "<<u<<endl;
+        }
+    }
+    for(int u : graph[1])
+        DFS2(u);
     return 0;
 }
