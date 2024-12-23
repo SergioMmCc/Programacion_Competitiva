d6c06b9f6f253da58aa298fd42c9b7fcf3a9d5a4
Sergio Moncada <sergio.moncada1@utp.edu.co>
2024-12-23 01:53:46 -0500

    Add some codeforces

--- CodeForces/Round 994 (Div 2)/D.cpp
+++ CodeForces/Round 994 (Div 2)/D.cpp
@@ -7,20 +7,56 @@ using ld = long double;
 #define sz size()
 
 const int maxn = 201;
-const ll inf = 1e5;
-ll matriz[maxn][maxn], table[maxn][maxn][maxn], dp[maxn][maxn][maxn];
+const ll inf = 1e15;
+ll matriz[maxn][maxn], dp[maxn][maxn];
+ll table[maxn][maxn][maxn], tableClean[maxn][maxn][maxn], tableOrder[maxn][maxn][maxn];
 
 void fillTable(ll n, ll m, ll k){
     for(int i = 1; i <= n; i++){
         for(int jl = 1; jl <= m; jl++){
             ll sum = 0; 
-            for(int jr = jl, count = 1; count <= m; jr++, count++){
-                if(jr == m + 1) jr = 1;
+            for(int len = 1; len <= m; len++){
+                int jr = jl + len - 1;
+                if(jr > m) jr -= m;
                 sum += matriz[i][jr];
-                table[i][jl][jr] = sum;
+                table[i][jl][len] = sum;
+            }
+        }
+
+        for(int len = 1; len <= m; len++){
+            ll minVal = inf; int minIndex = 0;
+            for(int jl = 1; jl <= m; jl++){
+                if(table[i][jl][len] < minVal){
+                    minVal = table[i][jl][len];
+                    minIndex = jl;
+                }
+            }
+
+            tableClean[i][minIndex][len] = minVal;
+            for(int jlAux = minIndex - 1, count = 1; count <= m; count++, jlAux--){
+                if(!jlAux) jlAux = m;
+                minVal = min(minVal + k, table[i][jlAux][len]);
+                tableClean[i][jlAux][len] = minVal;
+            }
+
+            for(int jl = 1; jl <= m; jl++){
+                int jr = jl + len - 1;
+                if(jr > m) jr -= m;
+                tableOrder[i][jl][jr] = tableClean[i][jl][len];
             }
         }
     }
+}
+
+ll calc(int n, int m, int i, int j){
+    if(dp[i][j] != inf) return dp[i][j];
+
+    if(i == 1) dp[i][j] = tableOrder[i][1][j];
+    else{
+        for(int k = j; k > 0; k--) dp[i][j] = min(dp[i][j], tableOrder[i][k][j] + calc(n, m, i - 1, k));
+    }
+    
+    return dp[i][j];
 }
 
 void solver(){
@@ -28,10 +64,8 @@ void solver(){
 
     // Limpiar dp
     for(int i = 1; i <= n; i++){
-        for(int jl = 1; jl <= m; jl++){
-            for(int jr = 1; jr <= m; jr++){
-                dp[i][jl][jr] = inf;
-            }
+        for(int j = 1; j <= m; j++){
+            dp[i][j] = inf;
         }
     }
 
@@ -44,17 +78,9 @@ void solver(){
 
     fillTable(n, m, k);
 
-    // Testear fillTable
-    for(int i = 1; i <= n; i++){
-        cout<<"Fila "<<i<<":"<<endl;
-        for(int jl = 1; jl <= m; jl++){
-            for(int jr = jl, count = 1; count <= m; jr++, count++){
-                if(jr > m) jr = 1;
-                cout<<table[i][jl][jr]<<' ';
-            }
-            cout<<endl;
-        }
-    }
+    ll ans = inf;
+    ans = calc(n, m, n, m);
+    cout<<ans<<endl;
 }
 
 int main(){
