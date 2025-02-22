5031c2c7ffc863b30a278b58b13189930ffe0462
Sergio Moncada <sergio.moncada1@utp.edu.co>
2025-02-22 00:58:02 -0500

    Fix exercise ITMO Curse DSU Step 3 C

--- CodeForces/ITMO Academy: pilot course/DSU/Step 3/C.cpp
+++ CodeForces/ITMO Academy: pilot course/DSU/Step 3/C.cpp
@@ -8,18 +8,19 @@ using ld = long double;
 typedef pair<int, int> pii;
 typedef pair<int, pii> piii;
 
-const int maxn = 300001;
+const int maxn = 300005;
 int components;
 vector<int> leader(maxn), sizen(maxn);
 vector<int> ans;
 
 struct change{
+    bool op;
     int u, len, from;
-};
+}; stack<change> st;
 
 struct query{
     bool op;
-    int l, r, u, v;
+    int t, l, r, u, v;
 };
 
 void initDSU(int n){
@@ -35,25 +36,26 @@ int find(int u){
     return find(leader[u]);
 }
 
-void join(int u, int v, stack<change>& st){
+void join(int u, int v){
     u = find(u); v = find(v);
     if(u != v){
         if(sizen[v] > sizen[u])
             swap(u, v);
         leader[v] = u;
-        st.push({v, sizen[u], u});
+        st.push({1, v, sizen[u], u});
         sizen[u] += sizen[v];
         components--;
     }
 }
 
-void rollback(stack<change>& st){
+void rollback(){
     while(!st.empty()){
+        bool op = st.top().op;
         int u = st.top().u;
         int len = st.top().len, from = st.top().from;
         st.pop();
 
-        if(!u) break;
+        if(!op) break;
         
         sizen[from] = len;
         leader[u] = u;
@@ -61,65 +63,57 @@ void rollback(stack<change>& st){
     }
 }
 
-void persist(stack<change>& st){
-    st.push({0, 0, 0});
+void persist(){
+    st.push({0, 0, 0, 0});
 }
 
-void work(int l, int r, queue<query>& q1, stack<change>& st){
-    // cout<<"l -> "<<l<<" r -> "<<r<<endl;
+void work(int l, int r, queue<query>& q1){
     if(l == r - 1){
-        persist(st);
+        persist();
         
         queue<query> q2;
         while(!q1.empty()){
             query q = q1.front(); q1.pop();
-            // cout<<q.op<<' '<<q.t<<' '<<q.l<<' '<<q.r<<' '<<q.u<<' '<<q.v<<endl;
-            if(!q.op && q.l == l) q2.push(q);
-            if(q.op && q.l <= l && q.r >= r) join(q.u, q.v, st);
+            if(!q.op && q.t == l) q2.push(q);
+            if(q.op && q.l <= l && q.r >= r) join(q.u, q.v);
         }
-        // cout<<"components -> "<<components<<endl;
         while(!q2.empty()){
             query q = q2.front(); q2.pop();
-            ans[q.l] = components;
+            ans[q.t] = components;
         }
-        rollback(st);
+        rollback();
         return;
     }
 
-    persist(st);
-    queue<query> q2, q3;
-    int m = (l + r) >> 1;
+    persist();
+    queue<query> q2;
     while(!q1.empty()){
         query q = q1.front(); q1.pop();
-        // cout<<q.op<<' '<<q.t<<' '<<q.l<<' '<<q.r<<' '<<q.u<<' '<<q.v<<endl;
-        if(q.op && q.l <= l && q.r >= r) join(q.u, q.v, st);
-        else{
-            if ((!q.op && q.l >= l && q.l < m) ||
-                (q.op && ((q.l >= l && q.l < m) || (q.r - 1 >= l && q.r <= m)))) q2.push(q);
-            if ((!q.op && q.l >= m && q.l < r) ||
-                (q.op && ((q.l >= m && q.l < r) || (q.r - 1 >= m && q.r <= r)))) q3.push(q);
-        }
+        if(q.op && q.l <= l && q.r >= r) join(q.u, q.v);
+        else if((!q.op && q.t >= l && q.t < r) ||
+            (q.op && ((q.l >= l && q.l < r) || (q.r - 1 >= l && q.r <= r)))) q2.push(q);
     }
 
-    work(l, m, q2, st);
-    work(m, r, q3, st);
-    rollback(st);
+    int m = (l + r) >> 1;
+    queue<query> q3 = q2;
+    work(l, m, q2);
+    work(m, r, q3);
+    rollback();
 }
 
 void solver(){
     int n, m; cin>>n>>m; if(!m) return;
     initDSU(n);
-    stack<change> st;
     map<pii, int> T;
     vector<query> queries;
     for(int i = 0; i < m; i++){
         char op; cin>>op;
-        if(op == '?') queries.pb({0, i, 0, 0, 0});
+        if(op == '?') queries.pb({0, i, 0, 0, 0, 0});
         else{
             int u, v; cin>>u>>v; if(u > v) swap(u, v);
             if(op == '+'){
                 T[{u, v}] = queries.sz;
-                queries.pb({1, i, -1, u, v});
+                queries.pb({1, 0, i, -1, u, v});
             }
             else queries[T[{u, v}]].r = i;
         }
@@ -130,11 +124,9 @@ void solver(){
         if(queries[i].op && queries[i].r == -1) queries[i].r = m;
         q1.push(queries[i]);
     }
-    queries.clear();
-    T.clear();
 
     ans.resize(m);
-    work(0, m, q1, st);
+    work(0, m, q1);
     for(int i = 0; i < m; i++){
         if(!ans[i]) continue;
         cout<<ans[i]<<endl;
