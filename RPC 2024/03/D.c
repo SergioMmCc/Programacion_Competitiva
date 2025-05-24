60a2870917bc87eba5b369b6c5be6d83a6beef05
SergioMmCc <sergio.moncada1@utp.edu.co>
2025-05-23 22:07:26 -0500

    Add exercise 2109D

--- RPC 2024/03/D.c
+++ RPC 2024/03/D.c
@@ -3,33 +3,34 @@
 #include <math.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#define int long long
 #define limite 10005
 
-long long int power (long long int base, int exponente) {
-    long long int num = 1;
+long long power (long long base, int exponente) {
+    long long num = 1;
 	while (exponente--)
 		num *= base;
 	return num;
 }
 
-int main() {
+signed main() {
     bool es_primo[limite + 1];
     int counter = 2, flag = 0;
-    long long int num, exponent, root;
+    long long num, exponent, root;
     scanf ("%lld", &num);
     
     es_primo[0] = es_primo[1] = false;
     for (int i = 2; i <= limite; ++i) 
         es_primo[i] = true;
     
-    for (long long int p = 2; p <= limite; p++) {
+    for (long long p = 2; p <= limite; p++) {
         if (es_primo[p]) {
             exponent = log (num) / log (p);
             if (power (p, exponent) == num && exponent % 2 != 0) {
                 flag = 1;
                 break;
             }
-            for (long long int i = p * p; i <= limite; i += p) 
+            for (long long i = p * p; i <= limite; i += p) 
                 es_primo[i] = false;
         }
     }
