8a1fd68f29fe7a244f158233a9880a9ea25e8741
SergioMmCc <sergio.moncada1@utp.edu.co>
2025-06-15 23:18:13 -0500

    Add DP LIS theory and exercises

--- CSES/DP/Increasing Subsequence.cpp
+++ CSES/DP/Increasing Subsequence.cpp
@@ -11,6 +11,20 @@ typedef pair<int, int> pii;
 
 const int inf = 1e9 + 1;
 
+// Complejidad: O(n * log(n))
+// - En este problema me piden el tamaño de la Longest Increasing
+//   Subsequence (LIS). Una LIS es un array a donde para cada
+//   i, j con i < j, a[i] < a[j].
+// - Para hallar una Longest Non-Decreasing Subsequence, es decir,
+//   el tamaño del mayor array a donde para cada i < j, a[i] <= a[j],
+//   solo hay que revisar la inecuacion de la busqueda binaria
+// - La Longest Decreasing Subsequence (LDS) es quivalente a el LIS
+//   de un array invertido
+// - Si me piden el menor numero de Increasing Subsequences, eso es
+//   equivalente al tamaño de la Longest Non-Increasing Subsequence.
+//   Es decir, el mayor tamaño de array a, donde para cada 
+//   i < j, a[i] >= a[j]
+
 void solver(){
     int n; cin>>n;
     vector<int> a(n+1);
@@ -23,6 +37,9 @@ void solver(){
         if(it == d.end()) continue;
 
         int index = distance(d.begin(), it);
+        // Este if se encarga de no poner numeros iguales
+        // Si me piden que cada numero sea estrictamente mayor
+        // Si piden mayor o igual se puede quitar
         if(a[i] > d[index - 1]) d[index] = a[i]; 
     }
 
