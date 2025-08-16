2149eb6dfae034791e222f4e5bf6037ad3023e23
SergioMmCc <sergio.moncada1@utp.edu.co>
2025-08-15 21:19:31 -0500

    Add some CSES Range Queries exercises

---
+++ CSES/Range Queries/Dynamic Range Sum Queries.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+using ll = long long;
+using ld = long double;
+#define pb push_back
+#define sz size()
+#define fi first
+#define se second
+typedef pair<int, int> pii;
+
+const int maxn = 2e5 + 1;
+
+// Implementacion con array 0-index
+// Todas las operaciones se hacen inicialmente con v = 1, tl = 0, tr = n - 1
+/*
+    Tambien podemos calcular multiplicaciones (incluyendo multiplicacion modular y
+    multiplicacion de matrices), operaciones bitwise como and (&), or (|) y xor (^),
+    gcd y cualquier operacion que cumpla con la propiedad asociativa
+*/ 
+vector<ll> tree(4*maxn);
+
+void build(vector<int>& a, int v, int tl, int tr){
+    if(tl == tr) tree[v] = a[tl];
+    else{
+        int tm = (tl + tr) / 2;
+        build(a, 2*v, tl, tm);
+        build(a, 2*v + 1, tm + 1, tr);
+        tree[v] = tree[2*v] + tree[2*v + 1];
+    }
+}
+
+// Las queries son [l, r]
+ll query(int v, int tl, int tr, int l, int r){
+    if(l > r) return 0;
+    if(tl == l && tr == r) return tree[v];
+
+    int tm = (tl + tr) / 2;
+    return query(2*v, tl, tm, l, min(tm, r))
+         + query(2*v + 1, tm + 1, tr, max(tm + 1, l), r);
+}
+
+void update(int v, int tl, int tr, int pos, ll val){
+    if(tl == tr) tree[v] = val;
+    else{
+        int tm = (tl + tr) / 2;
+        if(pos <= tm) update(2*v, tl, tm, pos, val);
+        else update(2*v + 1, tm + 1, tr, pos, val);
+        tree[v] = tree[2*v] + tree[2*v + 1];
+    }
+}
+
+void solver(){
+    int n, m; cin>>n>>m;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) cin>>a[i];
+
+    build(a, 1, 0, n-1);
+
+    while(m--){
+        int op; cin>>op;
+        if(op == 1){
+            int i, v; cin>>i>>v;
+            i--;
+            update(1, 0, n-1, i, v);
+        }
+        else{
+            int l, r; cin>>l>>r;
+            l--; r--;
+            cout<<query(1, 0, n-1, l, r)<<endl;
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(NULL);
+    int t = 1;
+    // cin>>t;
+    while(t--){
+        solver();
+    }
+
+    return 0;
+}
---
+++ CSES/Range Queries/Range Xor Queries.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+using ll = long long;
+using ld = long double;
+#define pb push_back
+#define sz size()
+#define fi first
+#define se second
+typedef pair<int, int> pii;
+
+const int maxn = 2e5 + 1;
+
+// Implementacion con array 0-index
+// Todas las operaciones se hacen inicialmente con v = 1, tl = 0, tr = n - 1
+/*
+    Tambien podemos calcular multiplicaciones (incluyendo multiplicacion modular y
+    multiplicacion de matrices), operaciones bitwise como and (&), or (|) y xor (^),
+    gcd y cualquier operacion que cumpla con la propiedad asociativa
+*/ 
+vector<ll> tree(4*maxn);
+
+void build(vector<int>& a, int v, int tl, int tr){
+    if(tl == tr) tree[v] = a[tl];
+    else{
+        int tm = (tl + tr) / 2;
+        build(a, 2*v, tl, tm);
+        build(a, 2*v + 1, tm + 1, tr);
+        tree[v] = tree[2*v] ^ tree[2*v + 1];
+    }
+}
+
+// Las queries son [l, r]
+ll query(int v, int tl, int tr, int l, int r){
+    if(l > r) return 0;
+    if(tl == l && tr == r) return tree[v];
+
+    int tm = (tl + tr) / 2;
+    return query(2*v, tl, tm, l, min(tm, r))
+         ^ query(2*v + 1, tm + 1, tr, max(tm + 1, l), r);
+}
+
+void solver(){
+    int n, m; cin>>n>>m;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) cin>>a[i];
+
+    build(a, 1, 0, n-1);
+
+    while(m--){
+        int l, r; cin>>l>>r;
+        l--; r--;
+        cout<<query(1, 0, n-1, l, r)<<endl;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(NULL);
+    int t = 1;
+    // cin>>t;
+    while(t--){
+        solver();
+    }
+
+    return 0;
+}
