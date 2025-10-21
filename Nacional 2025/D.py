8c0d4a1555248373b81c76840aae56ac9539c1fb
SergioMmCc <sergio.moncada1@utp.edu.co>
2025-10-21 14:40:45 -0500

    Add lectura hasta fin de archivo en Python

--- Nacional 2025/D.py
+++ Nacional 2025/D.py
@@ -16,39 +16,39 @@ def calcBinom():
             binom[i][j] = binom[i-1][j-1] + binom[i-1][j]
 
 def main():
-    read = input()
     while True:
-        #read = input()
-        numbers = read.split()
-        numbers = list(map(int, numbers))
-        n = numbers[0]
-        k = numbers[1]
+        try:
+            read = input()
+            numbers = read.split()
+            numbers = list(map(int, numbers))
+            n = numbers[0]
+            k = numbers[1]
 
-        if k == 0 and n == 0:
-            return
+            if k == 0 and n == 0:
+                return
 
-        l = 0
-        r = binom[n][k]
+            l = 0
+            r = binom[n][k]
 
-        read = input()
-        perm = read.split()
-        perm = list(map(int, perm))
-        last = 1
+            read = input()
+            perm = read.split()
+            perm = list(map(int, perm))
+            last = 1
 
-        for i in range(0, k):
-            for j in range(last, n+1):
-                # print(f"i {i}, j {j}")
-                if perm[i] == j:
-                    r = l + binom[n - j][k - i - 1]
-                    last = j + 1
-                    break
-                else:
-                    l += binom[n - j][k - i - 1]
+            for i in range(0, k):
+                for j in range(last, n+1):
+                    # print(f"i {i}, j {j}")
+                    if perm[i] == j:
+                        r = l + binom[n - j][k - i - 1]
+                        last = j + 1
+                        break
+                    else:
+                        l += binom[n - j][k - i - 1]
 
-        print(l)
-        read = input()
-        if len(read) == 0:
+            print(l)
+        
+        except EOFError:
             break
-if __name__ == '__main__':
-    calcBinom()
-    main()
+
+calcBinom()
+main()
