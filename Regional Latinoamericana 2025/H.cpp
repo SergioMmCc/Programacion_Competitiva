dacdd9768c6553d22c7485e3ed35868e2b41eebd
SergioMmCc <sergio.moncada1@utp.edu.co>
2025-11-17 23:09:56 -0500

    Add exercise H Regional Latinoamerica 2025

---
+++ Regional Latinoamericana 2025/H.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define pb push_back
+#define sz(a) ((int)a.size())
+#define all(a) a.begin(), a.end()
+#define fi first
+#define se second
+#define lb lower_bound
+#define ub upper_bound
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+// #include<ext/pb_ds/assoc_container.hpp>
+// using namespace __gnu_pbds;
+// using indexed_set = tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const int inf = 1e9 + 1;
+
+void solver(){
+    int n; cin>>n;
+    
+    pii ans = {0, 0};
+    for(int i = 0; i < n; i++){
+        int num; cin>>num;
+        if(num > ans.fi) ans = {num, ans.se + 1};
+    }
+    cout<<ans.se<<endl;
+}
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    // freopen("name.in", "r", stdin);
+	// freopen("name.out", "w", stdout);
+    int t = 1;
+    // cin>>t;
+    while(t--){
+        solver();
+    }
+
+    return 0;
+}
