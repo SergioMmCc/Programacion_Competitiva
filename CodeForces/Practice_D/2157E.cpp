3144d194e009e6fea0431148961cfd9f914e4382
SergioMmCc <sergio.moncada1@utp.edu.co>
2025-12-04 15:22:48 -0500

    Add exercise 2157E

---
+++ CodeForces/Practice_D/2157E.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define pb push_back
+#define sz(a) ((int)a.size())
+#define all(a) a.begin(), a.end()
+#define fi first
+#define se second
+#define lb lower_bound
+#define ub upper_bound
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+// #include<ext/pb_ds/assoc_container.hpp>
+// using namespace __gnu_pbds;
+// using indexed_set = tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solver(){
+    int n, k; cin>>n>>k;
+    vector<int> freq(4*n);
+    for(int i = 0; i < n; i++){
+        int num; cin>>num;
+        freq[num-1]++;
+    }
+
+    int st = 0, carry = 0, ans = 0;
+    for(int i = 0; i < 4*n; i++){
+        if(carry + freq[i] > k){
+            if(!carry) st = i;
+            carry += freq[i] - 1;
+        }
+        else{
+            carry = 0;
+            ans = max(ans, i - st);
+            st = i + 1;
+        }
+    }
+
+    cout<<ans<<endl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    // freopen("name.in", "r", stdin);
+	// freopen("name.out", "w", stdout);
+    int t = 1;
+    cin>>t;
+    while(t--){
+        solver();
+    }
+
+    return 0;
+}
