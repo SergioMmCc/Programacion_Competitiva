3705b788e8b52fd124c736f5eac931ffdd7a82f7
SergioMmCc <sergio.moncada1@utp.edu.co>
2026-05-31 17:40:29 -0500

    Add exercises Concert Tickets and Traffic Lights to CSES Sorting and Searching

---
+++ CSES/Sorting and Searching/Concert Tickets.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define pb push_back
+#define sz(a) ((int)a.size())
+#define all(a) a.begin(), a.end()
+#define fi first
+#define se second
+#define lb lower_bound
+#define ub upper_bound
+#define pqueue priority_queue
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+// #include<ext/pb_ds/assoc_container.hpp>
+// using namespace __gnu_pbds;
+// using indexed_set = tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solver(){
+    int n, m; cin>>n>>m;
+    multiset<int> ms;
+    for(int i = 0; i < n; i++){
+        int save; cin>>save;
+        ms.insert(save);
+    }
+
+    while(m--){
+        int lim; cin>>lim;
+        auto it = ms.ub(lim);
+        if(it == ms.begin()) cout<<-1<<endl;
+        else{
+            --it;
+            cout<<*it<<endl;
+            ms.erase(it);
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    // freopen("name.in", "r", stdin);
+	// freopen("name.out", "w", stdout);
+    int t = 1;
+    // cin>>t;
+    while(t--){
+        solver();
+    }
+
+    return 0;
+}
