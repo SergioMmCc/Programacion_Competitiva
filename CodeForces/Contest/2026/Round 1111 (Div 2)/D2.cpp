3df8df34993b7d1919681ce8c518aa7c7daaccf8
SergioMmCc <sergio.moncada1@utp.edu.co>
2026-07-19 22:13:28 -0500

    Add Round 1111

---
+++ CodeForces/Contest/2026/Round 1111 (Div 2)/D2.cpp
@@ -0,0 +1,140 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define db(x) cerr<< #x<<" "<<x<<endl
+#define for0(i,n) for(int i = 0; i < (int)n; i++)
+#define for1(i,n) for(int i = 1; i <= (int)n; i++)
+#define forlr(i,l,r) for(int i = (int)l; i <= (int)r; i++)
+#define forn1(i,n) for(int i = (int)n; i > 0; i--)
+#define forn0(i,n) for(int i = (int)(n) - 1; i >= 0; i--)
+#define forrl(i,l,r) for(int i = (int)r; i >= (int)l; i--)
+#define pb push_back
+#define sz(a) ((int)a.size())
+#define all(a) a.begin(), a.end()
+#define fi first
+#define se second
+#define lb lower_bound
+#define ub upper_bound
+#define pqueue priority_queue
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+// #include<ext/pb_ds/assoc_container.hpp>
+// using namespace __gnu_pbds;
+// using indexed_set = tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>;
+
+const ll INFL = 1000000000000000001;
+const int INF = 1e9 + 1;
+// const ll MOD = 1e9 + 7;
+
+struct node{ // Change
+    ll mini, maxi, val;
+};
+
+class segTree {
+private:
+    int size;
+    vector<node> tree;
+
+    node neutro = {LLONG_MAX - 1, LLONG_MIN + 1, 0LL}; // Change
+
+    node calcOp(node a, node b, ll len){ // Change
+        if(a.mini == neutro.mini) return b;
+        if(b.mini == neutro.mini) return a;
+        node ans;
+        ans.mini = min(a.mini, b.mini);
+        ans.maxi = max(a.maxi, b.maxi);
+        if(a.maxi > b.mini) ans.val = len;
+        else ans.val = max(a.val, b.val);
+        return ans;
+    }
+
+    void update(int pos, ll val, int v, int tl, int tr){ // O(lg(n))
+        if(tr - tl == 1){
+            tree[v] = {val, val, 0}; // Change
+            return;
+        }
+        
+        int tm = (tl + tr) / 2;
+        if(pos < tm) update(pos, val, 2*v + 1, tl, tm);
+        else update(pos, val, 2*v + 2, tm, tr);
+        tree[v] = calcOp(tree[2*v + 1], tree[2*v + 2], tm-tl);
+    }
+
+    // O(lg(n))
+    // [l, r)
+    node calc(int l, int r, int v, int tl, int tr){ // Change si walking on segment tree
+        if(tl >= r || l >= tr) return neutro;
+        if(tl >= l && tr <= r) return tree[v]; // si walking on seg tree -> tr - tl == 1
+
+        int tm = (tl + tr) / 2;
+        node m1 = calc(l, r, 2*v + 1, tl, tm);
+        node m2 = calc(l, r, 2*v + 2, tm, tr);
+        return calcOp(m1, m2, tm-tl);
+    }
+
+    void build(vl& a, int v, int tl, int tr){ // O(n)
+        if(tr - tl == 1){
+           if(tl < sz(a)) tree[v] = {a[tl], a[tl], 0}; // Change
+           return;
+        }
+        int tm = (tr + tl) / 2;
+        build(a, 2*v + 1, tl, tm);
+        build(a, 2*v + 2, tm, tr);
+        tree[v] = calcOp(tree[2*v + 1], tree[2*v + 2], tm-tl);
+    }
+
+
+public:
+    void init(int n){
+        size = 1;
+        while(size < n) size *= 2;
+        tree.assign(2*size, {0LL});
+        // build(a, 0, 0, size);
+    }
+
+    void update(int pos, ll val){
+        update(pos, val, 0, 0, size);
+    }
+
+    node calc(int l, int r){
+        return calc(l, r, 0, 0, size);
+    }
+
+    void build(vector<ll>& a){
+        build(a, 0, 0, size);
+    }
+};
+
+void solver(){
+    int n, q; cin>>n>>q;
+    vl a(n);
+    for0(i,n) cin>>a[i];
+    segTree st; st.init(n); st.build(a);
+    cout<<st.calc(0, n).val<<endl;
+    while(q--){
+        int idx; ll val; cin>>idx>>val;
+        st.update(idx, val);
+        cout<<st.calc(0, n).val<<endl;
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    // freopen("name.in", "r", stdin);
+	// freopen("name.out", "w", stdout);
+    int t = 1;
+    cin>>t;
+    while(t--){
+        solver();
+    }
+
+    return 0;
+}
