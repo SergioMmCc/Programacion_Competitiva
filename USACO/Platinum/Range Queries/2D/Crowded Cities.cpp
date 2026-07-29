43417180fbcf5904a42bbf6bb7552c5bc8eb6313
SergioMmCc <sergio.moncada1@utp.edu.co>
2026-07-28 21:07:11 -0500

    Update Crowded Cities

--- USACO/Platinum/Range Queries/2D/Crowded Cities.cpp
+++ USACO/Platinum/Range Queries/2D/Crowded Cities.cpp
@@ -23,9 +23,7 @@ typedef vector<pll> vll;
 // using namespace __gnu_pbds;
 // using indexed_set = tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>;
 
-// Logica correcta, demasiada memoria, solo pasa en casos con n, m <= 1000
-
-struct node{ // Change
+struct vertex{ // Change
     ll val; int idx;
 };
 
@@ -47,88 +45,107 @@ struct build{
     }
 };
 
-class segTree2D {
-private:
-    int size_n, size_m; // tree[n][m], n es 'y', m es 'x'
-    vector<vector<node>> tree;
+const int SZ = 1 << 13; // Techo del log2 del maximo indice
 
-    node neutro = {LLONG_MAX - 1}; // Change
+template<class T>
+T neutro(){
+    return {0, -1}; // Change
+}
 
-    node calcOp(node a, node b){ // Change
-        if(a.val == neutro.val) return b;
-        if(b.val == neutro.val) return a;
-        node ans;
-        if(a.val == b.val) ans = a.idx < b.idx ? a : b;
-        else ans = a.val > b.val ? a : b;
-        return ans;
-    }
+template<class T>
+T oper(T a, T b){
+    if(a.val == neutro<T>().val) return b;
+    if(b.val == neutro<T>().val) return a;
+    T ans;
+    if(a.val == b.val) ans = a.idx < b.idx ? a : b;
+    else ans = a.val > b.val ? a : b;
+    return ans;
+}
 
+template<class T> struct node {
+	T val = neutro<T>(); 
+    node<T>* c[2];
+	node() { c[0] = c[1] = NULL; }
 
-    // O(lg(n)*lg(m))
-    void update_x(int y, int x, node val, int yv, int ytl, int ytr, int xv, int xtl, int xtr){
-        if(xtr - xtl == 1){
-            if(ytr - ytl == 1) tree[yv][xv] = val;
-            else tree[yv][xv] = calcOp(tree[2*yv + 1][xv], tree[2*yv + 2][xv]);
+	void upd(int ind, T v, int L = 0, int R = SZ-1){ // asignar v
+		if(L == ind && R == ind){ 
+            val = v; 
+            return; 
         }
+		int M = (L+R) / 2;
+		if(ind <= M){
+			if(!c[0]) c[0] = new node();
+			c[0]->upd(ind,v,L,M);
+		} 
         else{
-            int xtm = (xtl + xtr) / 2;
-            if(x < xtm) update_x(y, x, val, yv, ytl, ytr, 2*xv + 1, xtl, xtm);
-            else update_x(y, x, val, yv, ytl, ytr, 2*xv + 2, xtm, xtr);
+			if(!c[1]) c[1] = new node();
+			c[1]->upd(ind,v,M+1,R);
+		}
+        T lv = c[0] ? c[0]->val : neutro<T>();
+        T rv = c[1] ? c[1]->val : neutro<T>();
+		val = oper(lv, rv);
+	}
 
-            tree[yv][xv] = calcOp(tree[yv][2*xv + 1], tree[yv][2*xv + 2]);
+    // [lo, hi]
+	T calc(int lo, int hi, int L = 0, int R = SZ-1){
+		if(hi < L || R < lo) return neutro<T>();
+		if(lo <= L && R <= hi) return val;
+		
+		int M = (L+R) / 2;
+        T lv = c[0] ? c[0]->calc(lo, hi, L, M) : neutro<T>();
+        T rv = c[1] ? c[1]->calc(lo, hi, M+1, R) : neutro<T>();
+		return oper(lv, rv);
+	}
+
+    // for 2D segtree
+	void UPD(int ind, node* c0, node* c1, int L = 0, int R = SZ-1){ 
+		if(L != R){
+			int M = (L+R) / 2;
+			if(ind <= M){
+				if(!c[0]) c[0] = new node();
+				c[0]->UPD(ind, c0 ? c0->c[0] : NULL, c1 ? c1->c[0] : NULL, L, M);
+			} 
+            else{
+				if(!c[1]) c[1] = new node();
+				c[1]->UPD(ind, c0 ? c0->c[1] : NULL, c1 ? c1->c[1] : NULL, M+1, R);
+			}
+		} 
+		val = oper((c0 ? c0->val : neutro<T>()), (c1 ? c1->val : neutro<T>()));
+	}
+};
+
+template<class T> struct Node {
+	node<T> seg; 
+    Node* c[2];
+	Node() { c[0] = c[1] = NULL; }
+
+	void upd(int x, int y, T v, int L = 0, int R = SZ-1){ // Asigna v en (x,y)
+		if(L == x && R == x){ 
+            seg.upd(y,v); 
+            return; 
         }
-    }
+		int M = (L+R) / 2;
+		if(x <= M){
+			if(!c[0]) c[0] = new Node();
+			c[0]->upd(x,y,v,L,M);
+		} 
+        else{
+			if (!c[1]) c[1] = new Node();
+			c[1]->upd(x,y,v,M+1,R);
+		}
+		seg.UPD(y, c[0] ? &c[0]->seg : NULL, c[1] ? &c[1]->seg : NULL);
+	}
 
-    void update_y(int y, int x, node val, int yv, int ytl, int ytr){
-        if(ytr - ytl != 1){
-            int ytm = (ytl + ytr) / 2;
-            if(y < ytm) update_y(y, x, val, 2*yv + 1, ytl, ytm);
-            else update_y(y, x, val, 2*yv + 2, ytm, ytr);
-        }
-
-        update_x(y, x, val, yv, ytl, ytr, 0, 0, size_m);
-    }
-
-
-    // O(lg(n)*lg(m))
-    // [yl, yr), [xl, xr)
-    node calc_x(int xl, int xr, int yv, int xv, int xtl, int xtr){
-        if(xtl >= xr || xl >= xtr) return neutro;
-        if(xtl >= xl && xtr <= xr) return tree[yv][xv];
-
-        int xtm = (xtl + xtr) / 2;
-        node m1 = calc_x(xl, xr, yv, 2*xv + 1, xtl, xtm);
-        node m2 = calc_x(xl, xr, yv, 2*xv + 2, xtm, xtr);
-        return calcOp(m1, m2);
-    }
-
-    node calc_y(int yl, int yr, int xl, int xr, int yv, int ytl, int ytr){
-        if(ytl >= yr || yl >= ytr) return neutro;
-        if(ytl >= yl && ytr <= yr) return calc_x(xl, xr, yv, 0, 0, size_m);
-
-        int ytm = (ytl + ytr) / 2;
-        node m1 = calc_y(yl, yr, xl, xr, 2*yv + 1, ytl, ytm);
-        node m2 = calc_y(yl, yr, xl, xr, 2*yv + 2, ytm, ytr);
-        return calcOp(m1, m2);
-    }
-
-
-public:
-    void init(int n, int m){
-        size_n = 1, size_m = 1;
-        while(size_n < n) size_n *= 2;
-        while(size_m < m) size_m *= 2;
-        tree.assign(2*size_n, vector<node>(2*size_m, {0, -1}));
-        // build(a, 0, 0, size_n);
-    }
-
-    void update(int y, int x, node val){
-        update_y(y, x, val, 0, 0, size_n);
-    }
-
-    node calc(int yl, int yr, int xl, int xr){
-        return calc_y(yl, yr, xl, xr, 0, 0, size_n);
-    }
+    // Query rectangle [(x1, y1), (x2, y2)]
+	T calc(int x1, int x2, int y1, int y2, int L = 0, int R = SZ-1){
+		if(x1 <= L && R <= x2) return seg.calc(y1,y2);
+		if(x2 < L || R < x1) return neutro<T>();
+		
+        int M = (L+R) / 2; 
+        T lv = c[0] ? c[0]->calc(x1, x2, y1, y2, L, M) : neutro<T>();
+        T rv = c[1] ? c[1]->calc(x1, x2, y1, y2, M+1, R) : neutro<T>();
+		return oper(lv, rv);
+	}
 };
 
 void solver(){
@@ -137,7 +154,6 @@ void solver(){
     vector<build> a(n);
     for(int i = 0; i < n; i++){
         cin>>a[i].l>>a[i].w>>a[i].h>>a[i].val;
-        assert(a[i].l <= 1000 && a[i].w <= 1000);
         if(a[i].l < a[i].w) swap(a[i].l, a[i].w);
         a[i].idx = i;
     }
@@ -145,21 +161,20 @@ void solver(){
     vi change(n);
     for(int i = 0; i < n; i++) change[a[i].idx] = i;
 
-    segTree2D st;
-    st.init(1001, 1001);
+    Node<vertex> st;
 
     vi where(n, -1);
 
     for(build b : a){
         int y = b.w, x = b.l, idx = b.idx; ll add = b.val;
-        node best = st.calc(y, 1001, x, 1001);
+        vertex best = st.calc(y, 5000, x, 5000);
         add += best.val;
         where[idx] = best.idx;
 
-        st.update(y, x, {add, idx});
+        st.upd(y, x, {add, idx});
     }
 
-    node ans = st.calc(0, 1001, 0, 1001);
+    vertex ans = st.calc(0, 5000, 0, 5000);
     cout<<ans.val<<endl;
 
     vi b;
